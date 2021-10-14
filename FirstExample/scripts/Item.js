76225a3891040e1d8a99668a32a45adb8a03c0fc
DanielB <d.banasiewicz@pdo.com.pl>
2021-10-14 19:22:40 +0200

    restaurant menu app is done

---
+++ FirstExample/scripts/Item.js
@@ -0,0 +1,8 @@
+const Item = (props) => (
+    <li
+        className={props.active? "enable" : "disabled"}
+        onClick={() => props.changeStatus(props.id)}
+    >
+        {props.name}
+    </li>
+)
